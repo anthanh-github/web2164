acd3c1dc99a97b5e32c77cdfea7df8af61258625
anthanh-github <84690458+anthanh-github@users.noreply.github.com>
2021-06-06 19:30:30 +0700

    Update 02.03.01_index.js

--- 02.03.01_index.js
+++ 02.03.01_index.js
@@ -4,40 +4,40 @@
 // Mở thư viện fs, express
 // const fs = require("fs");
 const express = require("express");
+var path = require('path');
 // Tham số - Web lắng nghe tại xport
 xport = process.env.PORT || 3000;
 // Tạo App Web
 const app = express();
 const router = express.Router();
 // Routing WEB
-// MCV
-
+// MVC
 
 //--- Home
 router.get('/',
     (req, res) => {
-     res.sendFile(__dirname + "home.html");
+        res.sendFile(__dirname + "/home.html");
     }
 );
 
 //--- Profile
 router.get('/profile',
     (req, res) => {
-       res.sendFile(__dirname + "./procfile.html");
+        res.sendFile(__dirname + "/profile.html");
     }
 );
 
 //--- Login
 router.get('/login',
     (req, res) => {
-         res.sendFile(__dirname + "./login.html");
+        res.sendFile(__dirname + "/login.html");
     }
 );
 
 //--- Logout
 router.get('/logout',
     (req, res) => {
-         res.sendFile("./view/logout.html");
+        res.sendFile(__dirname + "/logout.html");
     }
 );
 
